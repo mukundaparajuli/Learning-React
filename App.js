7114da8f7aa526a066b84fb80bb643e6cb353ec2
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2023-10-03 23:33:36 +0545

    Day 2 of learning React

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
-const heading1 = React.createElement("h1", {
+// HMR ->Hot Module Replacement
+// FWA->File Watcher Algorithm
+// written in C++
+// pracel cache
+
+// parcel
+// minifies the code
+// cleans the code
+// Dev and production build
+//  super fast build algorithms
+// Image Optimization
+// Cashing by Development
+// Compresses the files as well
+// Compatible with older version of browsers
+// HTTPS on dev machine
+// parcel manages the port number as well
+// we should put parcel.cache in our gitignore-->because anything  which wwe can generate in  browser we should keep that in gitignore
+// parcel uses consistent  hashing algorithm
+// Zero config
+// Transitive Dependency
+// created a server for you
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+  "h1",
+  {
     className: "heading1",
-}, "heading 1");
+  },
+  "heading 1 from parcel"
+);
 
-const heading2 = React.createElement("h2", {
+const heading2 = React.createElement(
+  "h2",
+  {
     className: "heading2",
-}, "heading 2");
+  },
+  "heading 2"
+);
 
-const container = React.createElement("div", {
-    id: "container"
-}, [heading1, heading2]);
-console.log(container)
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
+console.log(container);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // pasing a react element inside the root
-root.render(container)
+root.render(container);
